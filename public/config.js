926d82c9ecabcd680480fb3449a497f75fc1ba0a
Rohit Parulkar <rohit.parulkar@livingsocial.com>
2013-10-12 19:18:21 -0400

    change atOnce to 100

--- public/config.js
+++ public/config.js
@@ -2,13 +2,13 @@ var config = {
   title: 'Rohit Parulkar',
   navbar: [  ],
   posts: [ 
-           '6477950',
-           '6490814',
+           '6477950', //Why I'm Writing With Git
+           '6490814', //Writing Ideas
            '6886499', //Mumbai 10.17.2013
            '6955115', //In Search of Socialized Medicine
-           '6955146', //Sports Journalism Enters Pubert
+           '6955146', //Sports Journalism Enters Puberty
            '6955219' //My Most Important Friend Request
            ],
-  atOnce: 5,
+  atOnce: 100,
   footerText: ''
 };

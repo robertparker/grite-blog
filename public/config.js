306c3c5d53f66ac9d55e15943b5d9f6ea2b02f13
Rohit Parulkar <rohit.parulkar@livingsocial.com>
2013-10-12 17:46:19 -0400

    add facebook and twitter writings

--- public/config.js
+++ public/config.js
@@ -2,7 +2,13 @@ var config = {
   title: 'Rohit Parulkar',
   navbar: [  ],
   posts: [ 
-           '6477950','6490814','6886499'],
+           '6477950',
+           '6490814',
+           '6886499', //Mumbai 10.17.2013
+           '6955115', //In Search of Socialized Medicine
+           '6955146', //Sports Journalism Enters Pubert
+           '6955219' //My Most Important Friend Request
+           ],
   atOnce: 5,
   footerText: ''
 };
